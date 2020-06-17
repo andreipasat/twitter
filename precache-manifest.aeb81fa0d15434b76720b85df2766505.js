0f9ee6c965348228ed6c61cdf011693192d4c3b7
Andrei Pasat <pasat_andrei@yahoo.com>
2020-06-17 20:47:42 +0300

    Updates

---
+++ precache-manifest.aeb81fa0d15434b76720b85df2766505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53063f1498ee7400bc84813d7b7aec93",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "fc21a0022b7eb5023759",
+    "url": "/twitter/static/css/main.a9af5463.chunk.css"
+  },
+  {
+    "revision": "da879da9ac0fa834a3f3",
+    "url": "/twitter/static/js/2.1d2e8210.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter/static/js/2.1d2e8210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc21a0022b7eb5023759",
+    "url": "/twitter/static/js/main.569f593b.chunk.js"
+  },
+  {
+    "revision": "205d33963f02548664ba",
+    "url": "/twitter/static/js/runtime-main.98c250b1.js"
+  }
+]);
